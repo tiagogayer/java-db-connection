4f3145c4025e7b618da4930c3572aef96a0f8cde
Tiago Arruda Gayer <tiagotag91@gmail.com>
2020-03-31 17:51:20 -0300

    Obter informacoes no db e aprimoramento de excessoes

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,19 +1,39 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
 
 public class Program {
-
-	/**
-	 * Using MySQL Java Connector
-	 * https://dev.mysql.com/downloads/connector/j/5.1.html
-	 */
+	
 	public static void main(String[] args) {
 		
-		Connection conn = DB.getConnection();
-		DB.closeConnection();
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.createStatement();
+			
+			rs = st.executeQuery("SELECT * FROM department");
+		
+			while(rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
 		
 	}
 
